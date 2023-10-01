101afa17c1ae7a686c81d92c34341232ce14da4e
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-10-01 14:11:51 +0300

    add new components

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,22 @@
+<script>
+import Main from '@/Layouts/Main.vue';
+export default {
+    name: "Index",
+    layout: Main,
+}
+</script>
+
+<template>
+<div class="flex">
+    <div class="w-1/2 border border-gray-600">
+
+    </div>
+    <div class="w-1/2 border border-gray-600">
+
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
