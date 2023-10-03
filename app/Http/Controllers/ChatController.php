8a7b539c5b4eeaf356edc3fe4de212dea2924fd6
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2023-10-03 22:51:42 +0300

    add controller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Chat\StoreRequest;
 use App\Http\Resources\User\UserResource;
 use App\Models\User;
 
@@ -14,5 +15,10 @@ class ChatController extends Controller
 
         $users = UserResource::collection($users);
         return inertia('Chat/Index', compact('users'));
+    }
+
+    public function store(StoreRequest $request)
+    {
+      dd($request->all());
     }
 }
